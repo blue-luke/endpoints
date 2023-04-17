044ee58f8b93b7fea605d879408ad52c68da6e1f
Luke Blackburn <75164517+blue-luke@users.noreply.github.com>
2023-04-17 16:50:53 +0100

    feat: endpoitn in api, counting the bumber of api calls

--- timer-api/server.js
+++ timer-api/server.js
@@ -1,11 +1,29 @@
 const express = require('express');
 const app = express();
 const cors = require('cors'); 
+const { Counter, register } = require('prom-client'); 
 
 app.use(cors());
 
+// Create a new Prometheus counter
+const timeApiCounter = new Counter({
+  name: 'time_api_requests_total',
+  help: 'Total number of requests to /api/time',
+});
+
 app.get('/api/time', (req, res) => {
+  timeApiCounter.inc();
   res.json({ currentTime: new Date().toISOString() });
+});
+
+// Add the /metrics endpoint
+app.get('/metrics', async (req, res) => {
+  try {
+  res.set('Content-Type', register.contentType);
+  res.end(await register.metrics());
+  } catch (error) {
+    res.status(500).end(`Error collecting metrics: ${error.message}`);
+  }
 });
 
 const PORT = process.env.PORT || 3000;
